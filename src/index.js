f4823f1f5d0bcd10224f759a9d34f25070b21cce
chuks festus <cutogu@gmail.com>
2017-04-22 17:08:11 +0100

    added index.js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-console */
+
+import Person from './person';
+
+const chuks = new Person('chuks festus');
+
+console.log(chuks.intro());
---
+++ src/person.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-console */
+
+export default class {
+  constructor(name) {
+    this.name = name;
+  }
+
+  intro() {
+    return `meet ${this.name}`;
+  }
+}
